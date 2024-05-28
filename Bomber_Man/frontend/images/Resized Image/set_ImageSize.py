0ed338ad6c6b3c1634c554878196b93f46a9da4d
HSULW <113079390+HSULW@users.noreply.github.com>
2024-05-28 20:06:22 +0800

    set Brick, Stone and Grass icon

---
+++ Bomber_Man/frontend/images/Resized Image/set_ImageSize.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+def resize_image(input_path, output_path, size=(100, 100)):
+    
+    with Image.open(input_path) as img:
+        img = img.resize(size, Image.LANCZOS)
+        img.save(output_path)
+        print(f"save sucessful {output_path}")
+
+input_image_path = 'stones.png'
+output_image_path = 'reset_stones.png'
+
+resize_image(input_image_path, output_image_path)
